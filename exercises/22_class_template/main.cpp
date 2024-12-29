a49999ccfe268617cab3ebab46c10e63fdae0135
superDNY <1301070890@qq.com>
2024-12-29 01:28:19 -0800

    done

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "../exercise.h"
-
+#include <cstring>
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
 template<class T>
@@ -10,6 +10,11 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(auto i = 0u; i < 4; i++)
+        {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -22,12 +27,53 @@ struct Tensor4D {
     Tensor4D(Tensor4D &&) noexcept = delete;
 
     // 这个加法需要支持“单向广播”。
-    // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
-    // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
-    // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
-    // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
+    // 具体来说，others 可以具有与 this 不同的形状，形状不同的维度长度必须为 1。
+    // others 长度为 1 但 this 长度不为 1 的维度将发生广播计算。
+    // 例如，this 形状为 [1, 2, 3, 4]，others 形状为 [1, 2, 1, 4]，
+    // 则 this 与 others 相加时，3 个形状为 [1, 2, 1, 4] 的子张量各自与 others 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        bool broadcast[4];
+        for(auto i = 0u; i < 4; i++)
+        {
+            if(shape[i] != others.shape[i])
+            {
+                broadcast[i] = true;
+            }
+            else
+            {
+                broadcast[i] = false;
+            }
+        }
+        
+        auto dst = this->data;
+        auto src = others.data;
+        T* marks[4]{src};
+
+        for(auto i0 = 0u; i0 < shape[0]; i0++)
+        {
+            if(broadcast[0]) src = marks[0];
+            marks[1] = src;
+
+            for(auto i1 = 0u; i1 < shape[1]; i1++)
+            {
+                if(broadcast[1]) src = marks[1];
+                marks[2] = src;
+
+                for(auto i2 = 0u; i2 < shape[2]; i2++)
+                {
+                    if(broadcast[2]) src = marks[2];
+                    marks[3] = src;
+
+                    for(auto i3 = 0u; i3 < shape[3]; i3++)
+                    {
+                        if(broadcast[3]) src = marks[3];
+                        *dst++ += *src++;
+                    }     
+                }
+            }
+        }
+
         return *this;
     }
 };
